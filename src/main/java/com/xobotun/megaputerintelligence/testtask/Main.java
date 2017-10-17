ca938f10a8917e5df5f0d75c84b7bec3f415499d
Paul Maminov <xobotun@ya.ru>
2017-10-17 14:22:19 +0300

    Basic code that generates required array + Maven config.

---
+++ src/main/java/com/xobotun/megaputerintelligence/testtask/Main.java
@@ -0,0 +1,11 @@
+package com.xobotun.megaputerintelligence.testtask;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        // Объект, содержащий в себе миллион элементов, заданных по определённому правилу.
+        SortedFloatArray array = new SortedFloatArray();
+        array.GetData();
+    }
+}
