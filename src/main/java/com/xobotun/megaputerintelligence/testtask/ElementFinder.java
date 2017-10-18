57fdf8b9985bf65a622e0bd6e281b583d5e90e6c
Paul Maminov <xobotun@ya.ru>
2017-10-18 11:46:07 +0300

    Made ElementFinder to be interface, not abstract class.

--- src/main/java/com/xobotun/megaputerintelligence/testtask/ElementFinder.java
+++ src/main/java/com/xobotun/megaputerintelligence/testtask/ElementFinder.java
@@ -1,21 +1,21 @@
 package com.xobotun.megaputerintelligence.testtask;
 
-public abstract class ElementFinder {
+public interface ElementFinder {
     // Функция, реализующая суть класса.
     // @param `data` – входной массив из миллиона элементов.
     // @return Позиция первого элемента, у которого целая часть равна индексу.
-    public abstract int GetIndexOfDesiredElement(float[] data);
+    int GetIndexOfDesiredElement(float[] data);
 
     // Перегруженная версия `GetIndexOfDesiredElement`. Принимает неразвёрнутый контейнер массива из миллиона
     // элементов.
     // @see ElementFinder#GetIndexOfDesiredElement(float[] data)
-    public abstract int GetIndexOfDesiredElement(SortedFloatArray data);
+    int GetIndexOfDesiredElement(SortedFloatArray data);
 
     // Функция, проверяющая, является ли конкретный элемент искомым.
     // @param `position` – индекс элемента в массиве.
     // @param `data` – элемент массива, расположенный по этому индексу. /*@contract*/
     // @return `true`, если целая часть равна индексу. `false`, целая часть не равна индексу.
-    public static boolean IsElementDesired(int position, float data) {
+    default boolean IsElementDesired(int position, float data) {
         return position == (int) data;
     }
 }
